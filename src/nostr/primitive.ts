5c13968ed4fbe085b5a5f03b2d3555efe10fde31
Akiomi Kamakura <akiomik@gmail.com>
2023-05-30 21:34:19 +0900

    Add some kinds

--- src/nostr/primitive.ts
+++ src/nostr/primitive.ts
@@ -34,13 +34,23 @@ export namespace Nostr {
     Contacts = 3,
     EncryptedDirectMessage = 4,
     EventDeletion = 5,
+    Repost = 6,
     Reaction = 7,
+    BadgeAward = 8,
     ChannelCreation = 40,
     ChannelMetadata = 41,
     ChannelMessage = 42,
     ChannelHideMessage = 43,
     ChannelMuteUser = 44,
+    Blank = 255,
+    Report = 1984,
+    ZapRequest = 9734,
+    Zap = 9735,
+    RelayList = 10002,
     Auth = 22242,
+    BadgeDefinition = 30008,
+    ProfileBadge = 30009,
+    Article = 30023
   }
 
   export type TagName = `#${string}`;
