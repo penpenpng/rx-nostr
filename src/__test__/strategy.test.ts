bc06cd8faac32014e13ea08fe79567ac4fdf548e
penpenpng <poman1638m@gmail.com>
2023-07-02 10:47:42 +0900

    fix(test): remove only

--- src/__test__/strategy.test.ts
+++ src/__test__/strategy.test.ts
@@ -161,7 +161,7 @@ describe("Single relay case", () => {
     await expect(relay).toReceiveREQ("sub:0");
   });
 
-  test.only("[forward] If connection is abnormally closed, REQ will be retried.", async () => {
+  test("[forward] If connection is abnormally closed, REQ will be retried.", async () => {
     const req = createRxForwardReq("sub");
     const spy = spyEvent();
     rxNostr.use(req).pipe(spy.tap()).subscribe();
