5c7a66f31493a59167c605a1bb61a78eb39ce1d6
penpenpng <poman1638m@gmail.com>
2023-07-10 23:41:07 +0900

    feat: filterType operator

--- src/__test__/helper.ts
+++ src/__test__/helper.ts
@@ -26,6 +26,12 @@ export const faker = {
       event: faker.event(packetOrEvent?.event ?? packetOrEvent),
     };
   },
+  messagePacket(message: Nostr.ToClientMessage.Any): MessagePacket {
+    return {
+      from: "*",
+      message,
+    };
+  },
 };
 
 export function spySub(): {
