474ccb688e3e7957101d78f7b2f6bed32adbe0cd
penpenpng <poman1638m@gmail.com>
2023-07-15 13:45:33 +0900

    feat: filterBy() provides `not` option

--- src/helper.ts
+++ src/helper.ts
@@ -1,4 +1,30 @@
 /** Return a function that is lazily evaluated for since/until parameters of `LazyFilter`. */
+import Nostr from "nostr-typedef";
+
+import { LazyFilter } from "./packet";
+
 export function now(): number {
   return Math.floor(new Date().getTime() / 1000);
+}
+
+export function evalFilters(
+  filters: LazyFilter | LazyFilter[]
+): Nostr.Filter[] {
+  if ("length" in filters) {
+    return filters.map(evalFilter);
+  } else {
+    return [evalFilter(filters)];
+  }
+}
+
+function evalFilter(filter: LazyFilter): Nostr.Filter {
+  return {
+    ...filter,
+    since: filter.since ? evalLazyNumber(filter.since) : undefined,
+    until: filter.until ? evalLazyNumber(filter.until) : undefined,
+  };
+}
+
+function evalLazyNumber(lazyNumber: number | (() => number)): number {
+  return typeof lazyNumber === "number" ? lazyNumber : lazyNumber();
 }
