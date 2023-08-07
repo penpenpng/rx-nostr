64ce15b83b0f9de028cae40383da51ef066484b5
penpenpng <poman1638m@gmail.com>
2023-08-08 00:02:50 +0900

    fix: send() given specified params didn't work

--- src/nostr/event.ts
+++ src/nostr/event.ts
@@ -108,12 +108,15 @@ export function verify(event: Nostr.Event): boolean {
   }
 }
 
-export function ensureRequiredFields(
+function ensureRequiredFields(
   event: Partial<Nostr.Event>
 ): event is Nostr.Event {
+  if (typeof event.id !== "string") return false;
+  if (typeof event.sig !== "string") return false;
+  if (typeof event.kind !== "number") return false;
+  if (typeof event.pubkey !== "string") return false;
   if (typeof event.content !== "string") return false;
   if (typeof event.created_at !== "number") return false;
-  if (typeof event.pubkey !== "string") return false;
 
   if (!Array.isArray(event.tags)) return false;
   for (let i = 0; i < event.tags.length; i++) {
