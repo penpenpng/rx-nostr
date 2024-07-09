1b8458931140f165b34baf6a20ec8bab5c0a6b41
penpenpng <poman1638m@gmail.com>
2024-07-09 21:17:25 +0900

    fix crypto package for new verifier interface

--- packages/crypto/src/verifier.ts
+++ packages/crypto/src/verifier.ts
@@ -3,7 +3,8 @@ import * as Nostr from "nostr-typedef";
 import { verify } from "./crypto.js";
 
 export interface EventVerifier {
-  (params: Nostr.Event): boolean;
+  (params: Nostr.Event): Promise<boolean>;
 }
 
-export const verifier = (event: Nostr.Event) => verify(event);
+export const verifier: EventVerifier = async (event: Nostr.Event) =>
+  verify(event);
