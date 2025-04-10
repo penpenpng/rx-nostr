fda7e005d5f34447ace22631b936aceab24b154c
penpenpng <poman1638m@gmail.com>
2025-04-11 03:45:37 +0900

    chore: setup eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,39 @@
-import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
+import * as resolver from "eslint-import-resolver-typescript";
+import { flatConfigs as importx } from "eslint-plugin-import-x";
+import { defineConfig } from "eslint/config";
 import globals from "globals";
-import tseslint from "typescript-eslint";
-
+import ts from "typescript-eslint";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+  js.configs.recommended,
+  ts.configs.recommended,
+  importx.recommended,
+  importx.typescript,
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: { globals: globals.browser },
+  },
+  // import path must have `.ts` extension
+  // for a case that TypeScript is executed directly by Node.js, deno, etc.
+  {
+    rules: {
+      "import-x/extensions": ["error", "ignorePackages"],
+    },
+    settings: {
+      "import-x/extensions": [".ts", ".mts"],
+    },
+  },
+  {
+    settings: {
+      "import-x/resolver": {
+        name: "tsResolver",
+        resolver,
+      },
+    },
+  },
+  // eslint ignore
+  {
+    ignores: ["eslint.config.mjs", "dist/**", "node_modules/**"],
+  },
 ]);
