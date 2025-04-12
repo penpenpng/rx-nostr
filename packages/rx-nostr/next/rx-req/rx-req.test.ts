758154678b6814d56556b5da5c37aa3218348840
penpenpng <poman1638m@gmail.com>
2025-04-13 03:28:11 +0900

    test: refine RxReq tests

--- packages/rx-nostr/next/rx-req/rx-req.test.ts
+++ packages/rx-nostr/next/rx-req/rx-req.test.ts
@@ -14,24 +14,30 @@ test("RxReq emits a filter", async () => {
 
 test("Piped RxReq emits a filter", async () => {
   const rxq = new RxBackwardReq();
-  const obs = observe(rxq.pipe(filter(() => true)).packets$);
+  const obs = observe(rxq.pipe(filter((_, idx) => idx % 2 === 0)).packets$);
 
   rxq.emit({ kinds: [0] });
+  rxq.emit({ kinds: [1] });
+  rxq.emit({ kinds: [2] });
 
   await expect(obs.pop()).resolves.toEqual({ filters: [{ kinds: [0] }] });
+  await expect(obs.pop()).resolves.toEqual({ filters: [{ kinds: [2] }] });
 });
 
 test("Extended RxReq emits a filter", async () => {
   class RxCustomReq extends RxBackwardReq {
-    fetchKind0() {
-      this.emit({ kinds: [0] });
+    fetchByKind(kind: number) {
+      this.emit({ kinds: [kind] });
     }
   }
 
   const rxq = new RxCustomReq();
-  const obs = observe(rxq.pipe(filter(() => true)).packets$);
+  const obs = observe(rxq.pipe(filter((_, idx) => idx % 2 === 0)).packets$);
 
-  rxq.fetchKind0();
+  rxq.fetchByKind(0);
+  rxq.fetchByKind(1);
+  rxq.fetchByKind(2);
 
   await expect(obs.pop()).resolves.toEqual({ filters: [{ kinds: [0] }] });
+  await expect(obs.pop()).resolves.toEqual({ filters: [{ kinds: [2] }] });
 });
