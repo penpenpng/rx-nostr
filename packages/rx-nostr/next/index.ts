54cbdd0e9f38e822f6bc575301cb0a2919104c53
penpenpng <poman1638m@gmail.com>
2025-04-15 03:10:19 +0900

    feat: add index file for exports

---
+++ packages/rx-nostr/next/index.ts
@@ -0,0 +1,2 @@
+export { normalizeRelayUrl, RelayGroup, RelayMap, RelaySet, type RelayGroupUpdate } from "./libs/relay-collections.ts";
+export { RxBackwardReq, RxForwardReq, type IRxReq, type IRxReqPipeable, type RxReqStrategy } from "./rx-req/index.ts";
