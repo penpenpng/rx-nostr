85603a4a54af5d226a9e0ac308bef00cb6880896
penpenpng <poman1638m@gmail.com>
2025-04-16 01:22:29 +0900

    feat: export additional event-related functions and fetchRelayInfo from respective modules

--- packages/rx-nostr/next/index.ts
+++ packages/rx-nostr/next/index.ts
@@ -1,5 +1,7 @@
 export { Nip07Signer, NoopSigner, type EventSigner } from "./event-signer/index.ts";
 export { NoopVerifier, type EventVerifier } from "./event-verifier/index.ts";
+export { compareEvents, earlierEvent, ensureEventFields, laterEvent } from "./libs/nostr/event.ts";
+export { fetchRelayInfo } from "./libs/nostr/nip11.ts";
 export { normalizeRelayUrl, RelayGroup, RelayMap, RelaySet, type RelayGroupUpdate } from "./libs/relay-collections.ts";
 export { GlobalRelayDirectory, RelayDirectory, type IRelay, type IRelayDirectory } from "./relay-directory/index.ts";
 export { createRxNostr } from "./rx-nostr/index.ts";
