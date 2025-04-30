b870721154febbc0c632b87331bca7b30e67a9c5
penpenpng <poman1638m@gmail.com>
2025-04-30 18:20:12 +0900

    feat: add logger

---
+++ packages/rx-nostr/next/logger.ts
@@ -0,0 +1,38 @@
+let _logLevel: LogLevel = "error";
+
+type LogLevel = "debug" | "warn" | "error" | "silent";
+
+export function setLogLevel(logLevel: LogLevel) {
+  _logLevel = logLevel;
+}
+
+function logLevel() {
+  switch (_logLevel) {
+    case "debug":
+      return 0;
+    case "warn":
+      return 1;
+    case "error":
+      return 2;
+    case "silent":
+      return 3;
+  }
+}
+
+export class Logger {
+  static log(...args: unknown[]) {
+    if (logLevel() <= 0) {
+      console.log("[rx-nostr]", ...args);
+    }
+  }
+  static warn(...args: unknown[]) {
+    if (logLevel() <= 1) {
+      console.warn("[rx-nostr]", ...args);
+    }
+  }
+  static error(...args: unknown[]) {
+    if (logLevel() <= 2) {
+      console.error("[rx-nostr]", ...args);
+    }
+  }
+}
